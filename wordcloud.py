0321a4ff1f3b00f11a099d9dafadb2dd9dfe18d0
homesickjava <120647516@qq.com>
2020-04-27 04:18:18 +0800

    词云
    词云

---
+++ wordcloud.py
@@ -0,0 +1,17 @@
+import wordcloud
+
+# 从外部.txt文件中读取大段文本，存入变量txt中
+f = open('results.txt',encoding='utf-8')
+txt = f.read()
+
+# 构建词云对象w，设置词云图片宽、高、字体、背景颜色等参数
+w = wordcloud.WordCloud(width=1000,
+                        height=700,
+                        background_color='white',
+                        font_path='msyh.ttc')
+
+# 将txt变量传入w的generate()方法，给词云输入文字
+w.generate(txt)
+
+# 将词云图片导出到当前文件夹
+w.to_file('output3-sentence.png')
